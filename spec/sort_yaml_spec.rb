5d57370afc4996217a8921ae267993cf31f00dcb
Dorian Marié <dorian@dorianmarie.com>
2024-04-04 01:54:19 +0200

    linting

--- spec/sort_yaml_spec.rb
+++ spec/sort_yaml_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "spec_helper"
 require "tempfile"
 
@@ -5,18 +7,22 @@ RSpec.describe "sort-yaml" do
   it "works" do
     tempfile = Tempfile.create
     File.write(tempfile, <<~YAML)
-    b: 2
-    a: 1
+      b: 2
+      a: 1
     YAML
 
     `bin/sort-yaml #{tempfile.path}`
 
     expect(File.read(tempfile)).to eq(<<~YAML)
-    ---
-    a: 1
-    b: 2
+      ---
+      a: 1
+      b: 2
     YAML
   ensure
-    File.delete(tempfile) rescue nil
+    begin
+      File.delete(tempfile)
+    rescue StandardError
+      nil
+    end
   end
 end
